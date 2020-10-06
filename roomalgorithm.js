125dd6d3b41b1da01790ade442ae2278ff8b6777
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-05 22:34:04 -0400

    price algo working

--- roomalgorithm.js
+++ roomalgorithm.js
@@ -3,12 +3,16 @@
 
 function rentARoom(numFloors, numRooms) {
     let allRooms = [];
-    for (let i = 0; i < numFloors; i++) {
+    for (let i = 1; i <= numFloors; i++) {
         for (let j = 1; j <= numRooms; j++) {
-            allRooms.push({room: (i+1)*100+j})
+            allRooms.push({room: (i)*100+j,
+                           price: 50*i + (10*j-10) + 45,
+                           renter: null
+            
+            })
         }
     }
-    console.log(allRooms, allRooms.length)
+    console.log(allRooms)
 }
 
 rentARoom(4, 4);
