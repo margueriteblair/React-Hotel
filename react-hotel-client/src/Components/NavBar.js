2aeecaf5bf827929853839ee42e6bec968b9a8a2
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-07 17:55:20 -0400

    added data to the navbar

--- react-hotel-client/src/Components/NavBar.js
+++ react-hotel-client/src/Components/NavBar.js
@@ -3,39 +3,38 @@ import Button from './Button';
 import {RoomContext} from '../Contexts/RoomContext'
 
 export default function NavBar() {
-    const {setRooms} = useContext(RoomContext);
+    const {rooms, setRooms, bookedRooms, setBookedRooms} = useContext(RoomContext);
     let roomList = JSON.parse(localStorage.getItem("allAvailableRooms"));
-    let bookedRooms = [];
+    // const checkIn = () => {
+    //     for (let i = 0; i < roomList.length; i++) {
+    //         for (let j = 0; j < roomList[i].length; j++) {
+    //             if (roomList[i][j].room == document.querySelector("select").value) {
+    //                 roomList[i].splice(j, 1);
+    //             }
+    //         }
+    //     }
+    //     setRooms(roomList) //why does this need to be clicked twice to reset local storage
+    //     console.log(roomList)
+    // }
 
-    const checkIn = () => {
-        for (let i = 0; i < roomList.length; i++) {
-            for (let j = 0; j < roomList[i].length; j++) {
-                if (roomList[i][j].room == document.querySelector("select").value) {
-                    roomList[i].splice(j, 1);
-                }
-            }
-        }
-        setRooms(roomList) //why does this need to be clicked twice to reset local storage
-        console.log(roomList)
-    }
-
-    const checkOut = () => {
-        console.log("checking out")
-        console.log(document.querySelector("select").value)
-    }
+    // const checkOut = () => {
+    //     console.log("checking out")
+    //     console.log(document.querySelector("select").value)
+    // }
     return (
     <div style={{backgroundColor: "#4285f4"}}>
         <h1>Welcome to the CareerDevs Hotel, Coders!</h1>
+    <h3>There are currently {bookedRooms.flat().length} rooms booked, and {rooms.flat().length} available :)</h3>
 
             <div id="button-container" style={{justifyContent: "space-around"}}>
             <Button
             text="Book A Room!"
-            onClick={checkIn}
+            // onClick={checkIn}
             onClick={() => {window.location.href = '/checkin'}}
             />
             <Button
             text="Check Out"
-            onClick={checkOut}
+            // onClick={checkOut}
             onClick={() => {window.location = '/checkout'}}
             />
             </div>
