70180a8b9994e67b758bc889386358f19ca5665c
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-17 16:38:41 -0500

    navbar

--- react-hotel-client/src/Components/NavBar.js
+++ react-hotel-client/src/Components/NavBar.js
@@ -10,9 +10,8 @@ export default function NavBar() {
     <div style={{backgroundColor: "#4285f4"}}>
         <h1>Welcome to the CareerDevs Hotel, Coders!</h1>
     <h3>{bookedRooms.flat().length > 1 ? "There are currently " + bookedRooms.flat().length + " rooms" : "There is currently " + bookedRooms.flat().length + " room"} booked, and {rooms.flat().length} available :)</h3>
-    <h3>All rooms cost the price listed as well as a $25 deposit you will get upon checking out.</h3>
+    <h3>All rooms cost the price listed as well as a $25 deposit you will get back upon checking out.</h3>
     <h3>Your current balance is: ${balance}</h3>
-
             <div id="button-container" style={{justifyContent: "space-around"}}>
             <Button
             text="Book A Room!"
