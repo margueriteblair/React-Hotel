edd1c8f59d2b3ba84f48734e6d86ae588c7ed098
Marguerite Blair <marguerite.blair@gmail.com>
2021-01-18 15:26:43 -0500

    left notes about the local storage util

--- react-hotel-client/src/utils/localStorage.js
+++ react-hotel-client/src/utils/localStorage.js
@@ -5,7 +5,9 @@ module.exports = {
             return savedData;
         }
         if (initial instanceof Function) return initial();
-        return initial;
+        return initial; //in case it's not a function, but there is an initial value in there
+        //for instance, RoomContext makes use of a function to initialize, but MoneyContext starts off with a set
+        //$500
     },
     set: (key, value) => {
         localStorage.setItem(key, value)
